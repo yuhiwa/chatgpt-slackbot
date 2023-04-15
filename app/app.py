48e047282397f02a1ce0b36d0c30a738c178e43f
yuki.iwamoto <yuhiwa@gmail.com>
2023-04-16 00:51:25 +0900

    fix typo

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
-from slack_bolt import App from slack_bolt.adapter.socket_mode import SocketModeHandler import re import os import time
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import re
+import os
+import time
 import pickle
 import json
 import openai
